72dad464cc510d0751e9b98e4bc30004171ab492
kunshbisht <kunshbisht2011@gmail.com>
2025-12-25 00:17:05 +0100

    Bump pypm to v0.1.1

--- Formula/pypm.rb
+++ Formula/pypm.rb
@@ -1,7 +1,7 @@
 class Pypm < Formula
   desc "Simple Python package manager (pypm + pypx)"
   homepage "https://github.com/kunshbisht/pypm"
-  url "https://github.com/kunshbisht/pypm/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/kunshbisht/pypm/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "57b62a05b491049c56154df1450e77246108d9c8d2f9f6fcb768d8aec504703f"
   license "MIT"
 
