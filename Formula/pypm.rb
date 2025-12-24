4c30cc200cb988c99c3f7a2255213092dc7dff05
kunshbisht <kunshbisht2011@gmail.com>
2025-12-24 23:21:29 +0100

    Fix SHA256 checksum

--- Formula/pypm.rb
+++ Formula/pypm.rb
@@ -2,7 +2,7 @@ class Pypm < Formula
   desc "Simple Python package manager (pypm + pypx)"
   homepage "https://github.com/kunshbisht/pypm"
   url "https://github.com/kunshbisht/pypm/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "<SHA256_HASH_OF_TARBALL>"
+  sha256 "ee13d6db619999a885c08c0a1f6c6836517d06e9373c9385e30433ba3e0e151c"
   license "MIT"
 
   def install
